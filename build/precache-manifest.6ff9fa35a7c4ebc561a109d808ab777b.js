26be810ceb44c94e74185e39438b158571432403
Thawindu Angesh Tuto <angeshtuto@icloud.com>
2019-07-04 16:50:41 +0530

    Update favicon and build the app

---
+++ build/precache-manifest.6ff9fa35a7c4ebc561a109d808ab777b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37581c4e266c2f06624d4bb96089fab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ac9faa48e0620bc199",
+    "url": "/static/css/main.a2b7c5a9.chunk.css"
+  },
+  {
+    "revision": "15815284db2b2a6d3371",
+    "url": "/static/js/2.c77566d1.chunk.js"
+  },
+  {
+    "revision": "85ac9faa48e0620bc199",
+    "url": "/static/js/main.49e3e9c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
